118525118efa90b2174bc947027d047a56c15303
Denys Kravchenko <denys.kravchenko@globallogic.com>
2021-04-30 22:56:10 +0300

    CircleManager tests implementation

--- src/main/kotlin/model/Circle.kt
+++ src/main/kotlin/model/Circle.kt
@@ -1,23 +1,4 @@
 package model
 
-data class Circle(val id: Int, val name: String = "", val members: MutableList<Member> = mutableListOf<Member>()){
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+data class Circle(val id: Int, val name: String = "", val members: MutableList<Member> = mutableListOf<Member>())
 
-        other as Circle
-
-        if (id != other.id) return false
-        if (name != other.name) return false
-        if (members != other.members) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id
-        result = 31 * result + name.hashCode()
-        result = 31 * result + members.hashCode()
-        return result
-    }
-}
--- src/test/kotlin/CircleManagerTest.kt
+++ src/test/kotlin/CircleManagerTest.kt
@@ -1,5 +1,4 @@
 import model.Member
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
